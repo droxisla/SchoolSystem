cc375f7e05cd9f9452da2bed59fdf18c43b0b836
Drox <leandro.isla@orangeandbronze.com>
2012-06-21 16:16:26 +0800

    Set Subject load to 3 units

--- SchoolSystem/src/schoolsystem/model/Subject.java
+++ SchoolSystem/src/schoolsystem/model/Subject.java
@@ -4,10 +4,12 @@ public class Subject {
 	
 	private final String name;
 	private final Curriculum curriculum;
+	private final int load;
 	
 	public Subject(String name, Curriculum curriculum) {
 		this.name = name;
 		this.curriculum = curriculum;
+		load = 3;
 	}
 	
 	@Override
