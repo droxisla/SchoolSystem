b31b8e80a001c72cba83ec8ae8b3f2b8c3d4fbca
Drox <leandro.isla@orangeandbronze.com>
2012-06-26 14:02:08 +0800

    renamed tests to singular form

--- SchoolSystem/test/schoolsystem/model/AcademicYearTest.java
+++ SchoolSystem/test/schoolsystem/model/AcademicYearTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 import schoolsystem.model.schedule.*;
 
-public class AcademicYearTests {
+public class AcademicYearTest {
 
 	@Test
 	public void createCurrentAcademicTerm() {
--- SchoolSystem/test/schoolsystem/model/CurriculumTest.java
+++ SchoolSystem/test/schoolsystem/model/CurriculumTest.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 import org.junit.Test;
 
-public class CurriculumTests {
+public class CurriculumTest {
 
 	@Test
 	public void findAllPrerequisites() {
